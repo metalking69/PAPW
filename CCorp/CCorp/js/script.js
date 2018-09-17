956bc5001e09ae4e4cbd74c9d22d638e30337cfd
metalking69 <43329149+metalking69@users.noreply.github.com>
2018-09-16 22:36:24 -0500

    Add files via upload

---
+++ CCorp/CCorp/js/script.js
@@ -0,0 +1,27 @@
+/*$(document).ready(function(){
+  
+  var arrow=$(".arrow-up");
+  var form=$(".login-form");
+  var status = false;
+  event.preventDefault();
+    if(status == false){
+        arrow.fadeIn();
+        form.fadeIn();
+        status=true;
+    }
+    else{
+        arrow.fadeOut();
+        form.fadeOut();
+        status=false;
+    }
+
+})*/
+
+/*function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}*/
+
